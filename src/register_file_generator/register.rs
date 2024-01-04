78d56eabf35fa9e242780e46032ec7b557b11750
regerj <regerjacob@gmail.com>
2024-01-03 16:03:46 -0800

    simplify indentation

--- src/register_file_generator/register.rs
+++ src/register_file_generator/register.rs
@@ -46,51 +46,47 @@ impl Field {
     
     pub fn create_set_method(&self, register_width: u8) -> String {
         // Negative numbers need to be bounds checked differently
-        match self.negative {
-            true => {
-                format!(
-                    "\tinline bool set_{0}(int{3}_t value) {{\n\
-                    \t\tif (value < 0) {{\n\
-                    \t\t\tif (-value > (1 << ({2} - {1}))) {{\n\
-                    \t\t\t\treturn false;\n\
-                    \t\t\t}}\n\
-                    \t\t}} else {{\n\
-                    \t\t\tif (value >= (1 << ({2} - {1}))) {{\n\
-                    \t\t\t\treturn false;\n\
-                    \t\t\t}}\n\
-                    \t\t}}\n\
-                    \t\tuint{3}_t mask = static_cast<uint{3}_t>(~((UINT{3}_MAX >> ({3} - 1 - ({2} - {1}))) << {1}));\n\
-                    \t\tregister_raw &= mask;\n\
-                    \t\tvalue &= (UINT{3}_MAX >> ({3} - 1 - ({2} - {1})));\n\
-                    \t\tvalue = value << {1};\n\
-                    \t\tregister_raw |= value;\n\
-                    \t\treturn true;\n\
-                    \t}}\n",
-                    self.name,
-                    self.least_significant_bit,
-                    self.most_significant_bit,
-                    register_width
-                )
-            },
-            false => {
-                format!(
-                    "\tinline bool set_{0}(uint{3}_t value) {{\n\
-                    \t\tif (value >= (1 << ({2} - ({1} - 1)))) {{\n\
-                    \t\t\treturn false;\n\
-                    \t\t}}\n\
-                    \t\tuint{3}_t mask = static_cast<uint{3}_t>(~((UINT{3}_MAX >> ({3} - 1 - ({2} - {1}))) << {1}));\n\
-                    \t\tregister_raw &= mask;\n\
-                    \t\tvalue = value << {1};\n\
-                    \t\tregister_raw |= value;\n\
-                    \t\treturn true;\n\
-                    \t}}\n",
-                    self.name,
-                    self.least_significant_bit,
-                    self.most_significant_bit,
-                    register_width
-                )
-            },
+        if self.negative {
+            return format!(
+                "\tinline bool set_{0}(int{3}_t value) {{\n\
+                \t\tif (value < 0) {{\n\
+                \t\t\tif (-value > (1 << ({2} - {1}))) {{\n\
+                \t\t\t\treturn false;\n\
+                \t\t\t}}\n\
+                \t\t}} else {{\n\
+                \t\t\tif (value >= (1 << ({2} - {1}))) {{\n\
+                \t\t\t\treturn false;\n\
+                \t\t\t}}\n\
+                \t\t}}\n\
+                \t\tuint{3}_t mask = static_cast<uint{3}_t>(~((UINT{3}_MAX >> ({3} - 1 - ({2} - {1}))) << {1}));\n\
+                \t\tregister_raw &= mask;\n\
+                \t\tvalue &= (UINT{3}_MAX >> ({3} - 1 - ({2} - {1})));\n\
+                \t\tvalue = value << {1};\n\
+                \t\tregister_raw |= value;\n\
+                \t\treturn true;\n\
+                \t}}\n",
+                self.name,
+                self.least_significant_bit,
+                self.most_significant_bit,
+                register_width
+            );
+        } else {
+            return format!(
+                "\tinline bool set_{0}(uint{3}_t value) {{\n\
+                \t\tif (value >= (1 << ({2} - ({1} - 1)))) {{\n\
+                \t\t\treturn false;\n\
+                \t\t}}\n\
+                \t\tuint{3}_t mask = static_cast<uint{3}_t>(~((UINT{3}_MAX >> ({3} - 1 - ({2} - {1}))) << {1}));\n\
+                \t\tregister_raw &= mask;\n\
+                \t\tvalue = value << {1};\n\
+                \t\tregister_raw |= value;\n\
+                \t\treturn true;\n\
+                \t}}\n",
+                self.name,
+                self.least_significant_bit,
+                self.most_significant_bit,
+                register_width
+            )
         }
-        
     }
 }
