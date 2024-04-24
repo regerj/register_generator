456dd0eef64e3aeba5c96a0ebb969a827c1f92f9
regerj <regerjacob@gmail.com>
2024-04-23 18:29:15 -0700

    Dark mode :)

--- src/tui_handler.rs
+++ src/tui_handler.rs
@@ -13,10 +13,16 @@ use crate::reg_gen::register::*;
 use crate::reg_gen::json_handling::*;
 use crate::vertical_tab::*;
 
+// This file is responsible for all TUI operations. App stores the state of the TUI, and run_app
+// runs the TUI
+
+const BG_COLOR: Color = Color::Rgb(40, 44, 52);
+
 pub struct App {
     pub register_family: RegisterFamily,
     pub register_index: usize,
     pub field_index: usize,
+    pub field_selected: bool,
 }
 
 impl App {
@@ -25,6 +31,7 @@ impl App {
             register_family: pull_existing_json(path),
             register_index: 0,
             field_index: 0,
+            field_selected: false,
         }
     }
 
@@ -66,10 +73,36 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Resu
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q') => return Ok(()),
-                KeyCode::Right => app.next_register(),
-                KeyCode::Left => app.previous_register(),
-                KeyCode::Up => app.previous_field(),
-                KeyCode::Down => app.next_field(),
+                KeyCode::Right => {
+                    if app.field_selected {
+                        // Do nothing rn
+                    } else {
+                        app.next_register();
+                    }
+                },
+                KeyCode::Left => {
+                    if app.field_selected {
+                        // Do nothing rn
+                    } else {
+                        app.previous_register();
+                    }
+                },
+                KeyCode::Up => {
+                    if app.field_selected {
+                        // Do nothing rn
+                    } else {
+                        app.previous_field();
+                    }
+                },
+                KeyCode::Down => {
+                    if app.field_selected {
+                        // Do nothing rn
+                    } else {
+                        app.next_field();
+                    }
+                },
+                KeyCode::Enter => app.field_selected = true,
+                KeyCode::Esc => app.field_selected = false,
                 _ => {}
             }
         }
@@ -87,7 +120,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .split(size);
 
     // Full screen block I think
-    let block = Block::default().style(Style::default().bg(Color::White).fg(Color::Black));
+    let block = Block::default().style(Style::default().bg(BG_COLOR).fg(Color::White));
     f.render_widget(block, size);
 
     draw_register_tabs(f, app, chunks[0]);
@@ -101,17 +134,17 @@ fn draw_register_tabs<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: B
         .registers
         .iter()
         .map(|register| {
-            Spans::from(Span::styled(register.name.clone(), Style::default().fg(Color::Green)))
+            Spans::from(Span::styled(register.name.clone(), Style::default().fg(Color::White).bg(Color::Magenta)))
         })
         .collect();
     let tabs = Tabs::new(titles)
         .block(Block::default().borders(Borders::ALL).title("Tabs"))
         .select(app.register_index)
-        .style(Style::default().fg(Color::Cyan))
+        .style(Style::default().fg(Color::White))
         .highlight_style(
             Style::default()
                 .add_modifier(Modifier::BOLD)
-                .bg(Color::Black),
+                .bg(BG_COLOR),
         );
     f.render_widget(tabs, area);
 }
@@ -120,8 +153,8 @@ fn draw_register_view<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: B
     // Draw a box around the entire register view
     let block = Block::default()
         .style(Style::default()
-            .bg(Color::White)
-            .fg(Color::Black))
+            .bg(BG_COLOR)
+            .fg(Color::White))
         .borders(Borders::ALL)
         .title("Register View");
     f.render_widget(block, area);
@@ -140,7 +173,7 @@ fn draw_register_view<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: B
 fn draw_register_information<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: Backend {
     let text = vec![Spans::from(format!("Register Name: {}", app.register_family.registers[app.register_index].name)), Spans::from(format!("Size: {}-Bit", app.register_family.registers[app.register_index].size))];
     let paragraph = Paragraph::new(text.clone())
-        .style(Style::default().bg(Color::White).fg(Color::Black).add_modifier(Modifier::BOLD));
+        .style(Style::default().bg(BG_COLOR).fg(Color::White).add_modifier(Modifier::BOLD));
 
     f.render_widget(paragraph, area);
 }
@@ -148,8 +181,8 @@ fn draw_register_information<B>(f: &mut Frame<B>, app: &mut App, area: Rect) whe
 fn draw_field_view<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: Backend {
     let block = Block::default()
         .style(Style::default()
-            .bg(Color::White)
-            .fg(Color::Black))
+            .bg(BG_COLOR)
+            .fg(Color::White))
         .borders(Borders::ALL)
         .title("Field View");
 
@@ -173,17 +206,17 @@ fn draw_field_tabs<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: Back
         .fields
         .iter()
         .map(|field| {
-            Spans::from(Span::styled(field.name.clone(), Style::default().fg(Color::Green)))
+            Spans::from(Span::styled(field.name.clone(), Style::default().fg(Color::White).bg(Color::Magenta)))
         })
         .collect();
     let tabs = VerticalTabs::new(titles)
         .block(Block::default().borders(Borders::ALL).title("Fields"))
         .select(app.field_index)
-        .style(Style::default().fg(Color::Cyan))
+        .style(Style::default().fg(Color::White))
         .highlight_style(
             Style::default()
                 .add_modifier(Modifier::BOLD)
-                .bg(Color::Black),
+                .bg(BG_COLOR),
         );
     f.render_widget(tabs, area);
 }
@@ -197,7 +230,7 @@ fn draw_field_info<B>(f: &mut Frame<B>, app: &mut App, area: Rect) where B: Back
         Spans::from(format!("Write: {}", field.write)),
         Spans::from(format!("Negative: {}", if let Some(n) = field.negative {n} else {false}))];
     let paragraph = Paragraph::new(text.clone())
-        .style(Style::default().bg(Color::White).fg(Color::Black)).block(Block::default().borders(Borders::ALL).title("Field Information"));
+        .style(Style::default().bg(BG_COLOR).fg(Color::White)).block(Block::default().borders(Borders::ALL).title("Field Information"));
 
     f.render_widget(paragraph, area);
 }
